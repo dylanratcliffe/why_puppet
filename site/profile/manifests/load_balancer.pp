4e0c43f39186d5276a62d0b01a7f39d3085670d7
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-06-21 15:39:30 +1000

    Added Puppet config

--- site/profile/manifests/load_balancer.pp
+++ site/profile/manifests/load_balancer.pp
@@ -1,5 +1,6 @@
 #
 class profile::load_balancer {
+  # Set up the haproxy load balancer
   class { '::haproxy':
     global_options => {
       'user'  => 'root',
@@ -7,9 +8,24 @@ class profile::load_balancer {
     },
   }
 
+  # Create the load balancing pool
   haproxy::listen { 'spacetime':
     collect_exported => true,
     ipaddress        => $::ipaddress,
     ports            => '80',
+  }
+
+  # Make the Puppet agent run really often
+  ini_setting { 'puppet_run_frequency':
+    ensure  => present,
+    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    section => 'agent',
+    setting => 'runinterval',
+    value   => '30s',
+    notify  => Service['puppet'],
+  }
+
+  service { 'puppet':
+    ensure => running,
   }
 }
