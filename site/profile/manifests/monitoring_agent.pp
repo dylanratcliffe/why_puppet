3b8560d7e3d3bb27ff86d3f375185e7dd5a5ac1c
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-06-21 13:31:53 +1000

    Added collectd

---
+++ site/profile/manifests/monitoring_agent.pp
@@ -0,0 +1,20 @@
+#
+class profile::monitoring_agent {
+  class { '::collectd':
+    purge_config => true,
+    interval     => '5',
+  }
+
+  include ::collectd::plugin::cpu
+  include ::collectd::plugin::disk
+  include ::collectd::plugin::memory
+  include ::collectd::plugin::interface
+  include ::collectd::plugin::df
+
+  collectd::plugin::write_graphite::carbon {'my_graphite':
+    graphitehost   => 'metrics.methodologies.com',
+    graphiteport   => 2003,
+    graphiteprefix => '',
+    protocol       => 'tcp'
+  }
+}
--- site/profile/manifests/spacetime.pp
+++ site/profile/manifests/spacetime.pp
@@ -1,17 +1,17 @@
 #
 class profile::spacetime {
+  File {
+    owner => 'nginx',
+    group => 'nginx',
+    mode  => '0644',
+  }
+
   file { '/var/www/spacetime':
     ensure => directory,
-    owner  => 'nginx',
-    group  => 'nginx',
-    mode   => '0755',
   }
 
   file { '/var/www/spacetime/index.html':
     ensure => file,
-    owner  => 'nginx',
-    group  => 'nginx',
-    mode   => '0644',
     source => 'puppet:///modules/profile/index.html',
   }
 
